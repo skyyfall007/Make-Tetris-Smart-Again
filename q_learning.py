de440cb0adadbefa990d611f63537c23b48bf808
Reuben Massaquoi <Reubmann@gmail.com>
2018-02-25 20:57:29 -0500

    simple q-learning implementation to understand the concept

---
+++ q_learning.py
@@ -0,0 +1,63 @@
+#Q-learning example using a matrix representation of a node graph
+#The goal is to get to the state with the highest reward
+#the angent will traverse the graph by going from node to node until
+#they've reached the node with the highest reward.
+
+import random
+
+#Define reward table
+r =[[-1,-1,-1,-1,0,-1],
+    [-1,-1,-1,0,-1,100],
+    [-1,-1,-1,0,-1,-1],
+    [-1,0,0,-1,0,-1],
+    [0,-1,-1,0,-1,100],
+    [-1,0,-1,-1,0,100]]
+
+gamma =0.8
+
+#Define q-table filled with 0s 
+q = [[0 for x in range(6)]
+         for y in range(6)]
+
+n_episodes = 1
+n_states = 6
+
+c_state = None
+n_state = None
+c_action = None
+
+goal = False
+
+
+states = [n for n in range(n_states)]
+
+while n_episodes >0:
+    
+    n_episodes-=1
+    c_state = random.choice(states)
+    goal = False
+    
+    while not goal:
+        
+        available_actions = []
+        for i,e in enumerate(r[c_state]):
+            if e !=-1:
+                available_actions.append(i)
+                
+        c_action = random.choice(available_actions)
+        
+        q[c_state][c_action] = r[c_state][c_action]+gamma * max(q[c_action])
+        reward = r[c_state][c_action]
+        print(q)
+        if reward >=100:
+            goal = True
+        c_state =c_action
+
+
+        
+
+
+#Set initial state to random state
+#Set Current state to initial State
+#from current state, find action with the highest Q value
+#go to next state
